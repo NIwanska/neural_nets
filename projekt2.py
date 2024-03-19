d11ed47ba5449c8617ad1761b354452ac7c70f92
Natalia <natalia@pop-os.localdomain>
2024-03-19 17:48:58 +0100

    concat nie dziala

--- projekt2.py
+++ projekt2.py
@@ -7,16 +7,23 @@ import csv
 
 
 device = torch.device("cuda")
-
 data = pd.read_csv('train_data.csv')
-X = data.drop(columns=['SalePrice']).values
-Y = data['SalePrice'].values
+data_dropped = data.drop(columns=['HallwayType','HeatingType','AptManageType','SubwayStation', 'TimeToBusStop', 'TimeToSubway'])
+data_encoded = pd.concat([pd.get_dummies(data['HallwayType']),pd.get_dummies(data['HeatingType']),pd.get_dummies(data['AptManageType']), pd.get_dummies(data['SubwayStation']), pd.get_dummies(data['TimeToSubway']), pd.get_dummies(data['TimeToBusStop']), data_dropped])
+print(pd.get_dummies(data['HeatingType']))
+print(data_encoded)
+X = data_encoded.drop(columns=['SalePrice']).values
+Y = data_encoded['SalePrice'].values
+
 
 test_data = pd.read_csv('test_data.csv')
-test_data = test_data.astype('float32')
+test_data_dropped = test_data.drop(columns=['HallwayType','HeatingType','AptManageType','SubwayStation', 'TimeToBusStop', 'TimeToSubway'])
+test_data_encoded = pd.concat([pd.get_dummies(test_data['HallwayType']),pd.get_dummies(test_data['HeatingType']),pd.get_dummies(test_data['AptManageType']), pd.get_dummies(test_data['SubwayStation']), pd.get_dummies(test_data['TimeToSubway']), pd.get_dummies(test_data['TimeToBusStop']), test_data_dropped])
+
+test_data = test_data_encoded.astype('float32')
 test_data = torch.from_numpy(test_data.values[:,:])
 
-
+print(X)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 X_train = torch.tensor(X_train, dtype=torch.float32)
